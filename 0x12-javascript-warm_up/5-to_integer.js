c8240f44f457d94d589ed7ec7f49baf16757ab3e
iamqaasim <iamqaasim@gmail.com>
2022-11-28 13:00:05 -0800

    My first commit

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (typeof parseint(process.argv[2]) === 'string' || process.argv[2] === undefined) {
+if (typeof parseint(process.argv[2]) === undefined  || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
   console.log('My number:', parseInt(process.argv[2]));
