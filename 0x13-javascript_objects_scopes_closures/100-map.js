ddc9414c07d78c07df66d6c183f5068810a00a50
iamqaasim <iamqaasim@gmail.com>
2022-11-29 14:58:48 -0800

    My first commit

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const list2 = list.map(value, index => value * index);
+const list2 = list.map((value, index) => (value * index));
 console.log(list2);
